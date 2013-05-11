a249ace94df12a70b7e6e34514052f012eaa6d4b
Isao Yagi <isao@yahoo-inc.com>
2013-05-10 17:02:57 -0700

    +tests

---
+++ tests/docs.js
@@ -0,0 +1,65 @@
+var test = require('tap').test,
+    fn = require('../docs');
+
+
+function getEnv(args, opts) {
+    return {
+        cwd: '.',
+        args: args || [],
+        opts: opts || {},
+        app: {},
+        mojito: {}
+    };
+}
+
+test('mojito docs (no args)', function(t) {
+
+    function cb(err, msg) {
+        t.ok(err.message.match(/Unknown type/));
+    }
+
+    t.plan(1);
+    fn(getEnv(), cb)
+});
+
+test('mojito docs app (not an app dir)', function(t) {
+
+    function cb(err, msg) {
+        t.ok(err.message.match(/Not an application dir/));
+    }
+
+    t.plan(1);
+    fn(getEnv(['app']), cb)
+});
+
+test('mojito docs mojit (no name)', function(t) {
+
+    function cb(err, msg) {
+        t.ok(err.message.match(/Please specify mojit name/));
+    }
+
+    t.plan(1);
+    fn(getEnv(['mojit']), cb)
+});
+
+test('mojito docs mojit nonesuch', function(t) {
+
+    function cb(err, msg) {
+        t.ok(err.message.match(/Cannot find mojit nonesuch/));
+    }
+
+    t.plan(1);
+    fn(getEnv(['mojit', 'nonesuch']), cb)
+});
+
+test('mojito docs mojito', function(t) {
+
+    function cb(err, msg) {
+        console.log(err);
+        t.ok(err.message.match(/Cannot find the Mojito library/));
+    }
+
+    t.plan(1);
+    fn(getEnv(['mojito']), cb)
+});
+
